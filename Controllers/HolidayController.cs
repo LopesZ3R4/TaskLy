b8b0d24efd2129a2ee111c45cdd5e085bab6dc5b
LopesZ3R4 <jl.moreno7@hotmail.com>
2023-11-02 22:54:51 -0400

    Auth on holiday

--- Controllers/HolidayController.cs
+++ Controllers/HolidayController.cs
@@ -1,7 +1,9 @@
 // Path: c:\TaskLy\Controllers\HolidayController.cs
 using Data;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
+[Authorize]
 [Route("api/[controller]")]
 [ApiController]
 public class HolidayController : ControllerBase
