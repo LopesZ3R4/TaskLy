9e5cb9fde3eaf3077d999c4f290601781a12dc23
LopesZ3R4 <jl.moreno7@hotmail.com>
2023-11-06 21:08:55 -0400

    removed navigation properties

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -17,16 +17,6 @@ public class ApplicationDbContext : DbContext
 
         modelBuilder.Entity<TaskTags>()
         .HasKey(tt => new { tt.TaskId, tt.TagId, tt.Owner });
-
-        modelBuilder.Entity<TaskTags>()
-            .HasOne(tt => tt.Tasks)
-            .WithMany(t=> t.TaskTags)
-            .HasForeignKey(tt => tt.TaskId);
-
-        modelBuilder.Entity<TaskTags>()
-            .HasOne(tt => tt.Tags)
-            .WithMany(t=> t.TaskTags)
-            .HasForeignKey(tt => new { tt.TagId, tt.Owner });
     }
     public DbSet<User> Users { get; set; }
     public DbSet<Tasks> Tasks { get; set; }
